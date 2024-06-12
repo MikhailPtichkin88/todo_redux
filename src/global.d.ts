ed7a9a04153a1873914387ca298a26291ea59bac
MikhailPtichkin <mikhailptichkin1988@gmail.com>
2024-06-12 21:43:55 +0300

    init

---
+++ src/global.d.ts
@@ -0,0 +1,18 @@
+declare module '*.module.scss' {
+  const styles: { [className: string]: string };
+  export default styles;
+}
+
+declare module "*.svg" {
+  import { FC, SVGProps } from "react";
+  const content: FC<SVGProps<SVGElement>>;
+  export default content;
+}
+
+
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.png';
+declare module '*.gif';
+
+declare const __ENV__: "development" | "production"
---
+++ src/pages/Shop/Spop.lazy.tsx
@@ -0,0 +1,3 @@
+import React from "react";
+
+export const ShopLazy = React.lazy(()=>import("./Shop"))
