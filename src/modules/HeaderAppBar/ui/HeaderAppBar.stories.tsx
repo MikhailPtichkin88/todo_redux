988be15f34010681c861c63842ac4823fed442d3
MikhailPtichkin <mikhailptichkin1988@gmail.com>
2024-07-05 00:43:14 +0300

    fist story add

--- src/modules/HeaderAppBar/ui/HeaderAppBar.stories.tsx
+++ src/modules/HeaderAppBar/ui/HeaderAppBar.stories.tsx
@@ -1,6 +1,8 @@
 import { StoryFn } from '@storybook/react'
 import { HeaderAppBar } from './HeaderAppBar'
 import { BrowserRouter } from 'react-router-dom'
+import { useUserStore } from '../store/useUserStore'
+import { useEffect } from 'react'
 
 export default {
   title: 'Components/HeaderAppBar',
@@ -14,6 +16,18 @@ export default {
   ],
 }
 
-const Template = () => <HeaderAppBar />
+const Template = () => {
+  const { setUserData } = useUserStore()
+  useEffect(() => {
+    setUserData({
+      id: '999',
+      username: 'mockUser',
+      email: 'mockUser@example.com',
+      avatar:
+        'https://img.freepik.com/premium-vector/avatar-icon0002_750950-43.jpg',
+    })
+  }, [])
+  return <HeaderAppBar />
+}
 
 export const Default = Template.bind({})
